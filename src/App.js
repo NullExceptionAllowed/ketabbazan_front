a2e305d9a49eb5ae88cb76f6d582aa1c77663cc8
FatiAs <fatemehaskarijirhandeh@gmail.com>
2022-04-03 16:44:45 +0430

    example showbook

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Header from "./Components/Header/Header";
 import { Router } from "react-router";
 import Profile from "./Components/Profile/Profile";
 import Nav from "./Components/Navbar/Nav";
- import BasicGrid1 from "./Components/emtehani/emtehani";
+import BasicGrid1 from "./Components/emtehani/emtehani";
+import Emtehani from "./Components/emtehani/emtehani";
+
 
 const App = () => {
   return (
-    //  <BasicGrid1/>
-      <Switch>
-        <Route path="/" exact component={Nav} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/signup"  component={SignUp} />
-        <Route path="/profile"  component={Profile} />
-      </Switch>
+    <Emtehani/>
+      // <Switch>
+      //   <Route path="/" exact component={Nav} />
+      //   <Route path="/login" exact component={Login} />
+      //   <Route path="/signup"  component={SignUp} />
+      //   <Route path="/profile"  component={Profile} />
+      // </Switch>
   );
 };
 export default App;
--- src/Components/emtehani/emtehani.jsx
+++ src/Components/emtehani/emtehani.jsx
@@ -3,47 +3,58 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import ButtonBase from "@mui/material/ButtonBase";
+import book from "../../assets/Image/book.jpg";
+import { fontSize } from "@mui/system";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-function FormRow() {
+const emtehani = () => {
+  const Img = styled("img")({
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  });
   return (
-    <React.Fragment>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>Item</Item>
-      </Grid>
-    </React.Fragment>
-  );
-}
+    <div style={{direction:"rtl" ,display: "flex" , marginRight:"40px" }}>
+      <Paper
+        sx={{
+          direction: "rtl",
+          p: 2,
+          maxWidth: 180,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+          marginTop: "50px",
+          
+        }}
+      >
+        <Grid container spacing={2}  sx={{marginLeft:"40px"}}>
+          <Grid item>
+            <ButtonBase sx={{ width: 150, height: 150 }}>
+              <Img alt="complex" src={book} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs sx={{marginRight:"20px"}}>
+                <Typography gutterBottom variant="subtitle1" component="div" >
+                کتاب با عشق کورت 
+                </Typography>
+                <Typography variant="body2" gutterBottom style={{color:"#757C86" ,fontSize:"13px"}}>
+                  کورت ونه گات
+                </Typography>
 
-export default function BasicGrid() {
-  return (
-    <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={8}>
-          <Item>xs=6 md=8</Item>
+                <Typography variant="body2" gutterBottom style={{color:"#4DB200", fontSize:"17px"}}>
+                 310,000 ریال
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>xs=6 md=4</Item>
-        </Grid>
-        <Grid item xs={6} md={4}>
-          <Item>xs=6 md=4</Item>
-        </Grid>
-        <Grid item xs={6} md={8}>
-          <Item>xs=6 md=8</Item>
-        </Grid>
-      </Grid>
-    </Box>
+      </Paper>
+    </div>
   );
-}
+};
+
+export default emtehani;
