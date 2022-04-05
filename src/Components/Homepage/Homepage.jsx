0396a532ce5f587df00c3db76da32c0350945282
FatiAs <fatemehaskarijirhandeh@gmail.com>
2022-04-05 08:06:57 +0430

    add homepage

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Nav';
+import Header from '../Header/Header';
+import Showbook from "../Showbook/showbook";
+const Homepage = () => {
+    return ( 
+        <div>
+            <Navbar/>
+            <Header/>
+            <Showbook/>
+        </div>
+     );
+}
+ 
+export default Homepage;
--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -10,57 +10,15 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/Image/logo.png";
-import Home from "../../assets/Image/home.jpg";
+import Home from "../../assets/Image/home3.jpg";
 import Showbook from "../Showbook/showbook";
 import Box from "@mui/material/Box";
-import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
 import ArticleIcon from "@mui/icons-material/Article";
 import DrawerComp from "./DrawerCopm";
 
 const Nav = () => {
   const theme = useTheme();
   const isMatch = useMediaQuery(theme.breakpoints.down("sm"));
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: "Gray",
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.black, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "20ch",
-      },
-    },
-  }));
-
   const MenuButton = ({ Icon, text, linkTo }) => {
     return (
       <div style={{ marginBottom: "5 px" }}>
@@ -199,10 +157,9 @@ const Nav = () => {
         src={Home}
         alt="homeimg"
         style={{
-          height: "500px",
+          height: "600px",
           width: "100%",
           marginTop: "60px",
-          borderRadius: "1%",
         }}
       />
       <Typography
