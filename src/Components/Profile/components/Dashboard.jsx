a3aa2ad89d66bc744718135ac71ef1adb454d54b
unknown <amirhosseinizadi79@gmail.com>
2022-04-12 12:28:02 +0430

    test3

--- src/Components/Profile/components/Dashboard.jsx
+++ src/Components/Profile/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../Profile.css"; 
+import "../Profile.css";
 import Box from '@mui/material/Box';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { editableInputTypes } from "@testing-library/user-event/dist/utils";
@@ -9,84 +9,83 @@ import CakeIcon from '@mui/icons-material/Cake';
 import AbcIcon from '@mui/icons-material/Abc';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-const Dashboard = () =>
-{
-   const [username,setusername]=useState("");
+const Dashboard = () => {
+   const [username, setusername] = useState("");
 
-   const getItem =  ()  => {
 
-      axios.get('http://127.0.0.1:8000/profile/info/').then((res)=> {
 
-         console.log(res.data.username);
-         setusername(res.data.username);
-      })
+   axios.get('http://127.0.0.1:8000/profile/info/').then((res) => {
 
-   }
+      console.log(res.data.username);
+      setusername(res.data.username);
+   })
 
-   
-    return(
-    <div>
-    <div className="section">
-    <div className="title">
-      <p style={{fontSize:"18px"}}>اطلاعات حساب کاربری</p>
-    </div>
-     <div className="content" style={{paddingTop:"10px"}}>
-    <div
-style={{direction:"rtl"  ,  paddingRight:"20px", float:"right"}}
->
 
-   <AccountBoxIcon style={{float: "right"}}/>
-      <p style={{float: "right" , marginRight:"7px", fontWeight:"bold"}}>{username}  </p>           
+
+
+   return (
+      <div>
+         <div className="section">
+            <div className="title">
+               <p style={{ fontSize: "18px" }}>اطلاعات حساب کاربری</p>
+            </div>
+            <div className="content" style={{ paddingTop: "10px" }}>
+               <div
+                  style={{ direction: "rtl", paddingRight: "20px", float: "right" }}
+               >
+
+                  <AccountBoxIcon style={{ float: "right" }} />
+                  <p style={{ float: "right", marginRight: "7px", fontWeight: "bold" }}>{username}  </p>
+               </div>
+
+               <div
+                  style={{ direction: "rtl", paddingRight: "20px", float: "right", marginRight: "300px" }}
+               >
+                  <CakeIcon style={{ float: "right" }} />
+                  <p style={{ float: "right", marginRight: "7px", fontWeight: "bold" }}>تاریخ تولد : </p>
+               </div>
+
+
+               <br />
+               <br />
+               <div
+                  style={{ direction: "rtl", paddingRight: "20px" }}
+               >
+                  <EmailIcon style={{ float: "right" }} />
+                  <p style={{ float: "right", marginRight: "7px", fontWeight: "bold" }}>ایمیل :  </p>
+               </div>
+
+
+
+
+               <div
+                  style={{ direction: "rtl", paddingRight: "20px", float: "right", marginRight: "325px" }}
+               >
+                  <AccountCircleIcon style={{ float: "right" }} />
+                  <p style={{ float: "right", marginRight: "7px", fontWeight: "bold" }}>نام کاربری : </p>
+               </div>
+
+
+
+               <br />
+               <br />
+               <div
+                  style={{ direction: "rtl", paddingRight: "20px" }}
+               >
+                  <AbcIcon style={{ float: "right", }} />
+                  <p style={{ float: "right", marginRight: "7px", fontWeight: "bold" }}>بیوگرافی: </p>
+               </div>
+
+
+
+
+
+
+            </div>
+         </div>
+
       </div>
-
-      <div
-style={{direction:"rtl"  ,  paddingRight:"20px", float:"right" , marginRight:"300px"}}
->
-   <CakeIcon style={{float: "right"}}/>
-      <p style={{float: "right" , marginRight:"7px", fontWeight:"bold"}}>تاریخ تولد : </p>           
-      </div>
- 
-
-   <br/>
-   <br/>
-  <div
-style={{direction:"rtl"  ,  paddingRight:"20px"}}
->
-   <EmailIcon style={{float: "right"}}/>
-      <p style={{float: "right" , marginRight:"7px", fontWeight:"bold"}}>ایمیل :  </p>           
-      </div>
-
-      
-
-
-      <div
-style={{direction:"rtl"  ,  paddingRight:"20px", float:"right" , marginRight:"325px"}}
->
-   <AccountCircleIcon style={{float: "right"}}/>
-      <p style={{float: "right" , marginRight:"7px", fontWeight:"bold" }}>نام کاربری : </p>           
-      </div>
- 
-
-
-      <br/>
-   <br/>
-  <div
-style={{direction:"rtl"  ,  paddingRight:"20px"}}
->
-   <AbcIcon style={{float: "right" , }}/>
-      <p style={{float: "right" , marginRight:"7px", fontWeight:"bold"}}>بیوگرافی: </p>           
-      </div>
-
-      
-
- 
-    
-
-      </div>
-      </div>
-
-      </div>
-    )
+   )
 
 }
 
