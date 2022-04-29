ca5328efe19bcc161b31e13583af6c1e91b9b5ad
FatiAs <fatemehaskarijirhandeh@gmail.com>
2022-04-29 16:28:41 +0430

    fix pageination search

--- src/Components/Showbookall-search/Showbookall.jsx
+++ src/Components/Showbookall-search/Showbookall.jsx
@@ -24,52 +24,71 @@ import {
 import ChangeNav from "./../Navbar/changeNav";
 import Pagination from "@mui/material/Pagination";
 
-
 const Showbook = () => {
   const location = useLocation();
   const search = location.search;
-  console.log(search);
+  // console.log(search);
   const searchUrlParam = new URLSearchParams(search).get("q");
-  console.log(searchUrlParam);
+  // console.log(searchUrlParam);
   const [apiLoading, setApiLoading] = useState(false);
   const [bookinfo, setbookinfo] = useState([]);
   const [pagenum, setpagenum] = useState(1);
+  const [numpage, setnumpage] = useState(1);
+
+  const [pagenum2, setpagenum2] = useState(1);
+  const [numpage2, setnumpage2] = useState(1);
+
   const history = useHistory();
   useEffect(() => {
-    console.log("**");
     if (searchUrlParam === null) {
       setApiLoading(true);
       axios({
+        url: `${baseUrl}/read_book/all_books/page_count`,
+      }).then((response) => {
+        setnumpage(response.data);
+      });
+      axios({
         url: `${baseUrl}/read_book/all_books/${pagenum}`,
       }).then((response) => {
-        console.log(response.data);
         setbookinfo(response.data);
         setApiLoading(false);
       });
     } else {
       setApiLoading(true);
       axios({
-        url: `${baseUrl}/search/?q=${searchUrlParam}`,
+        url: `${baseUrl}/search/?q=${searchUrlParam}&page=page_count`,
       }).then((response) => {
+        setnumpage2(response.data);
         console.log(response.data);
+      });
+
+      axios({
+        url: `${baseUrl}/search/?q=${searchUrlParam}&page=${pagenum2}`,
+      }).then((response) => {
         setbookinfo(response.data);
-        setApiLoading(false);
         setApiLoading(false);
       });
     }
-  }, [pagenum, search]);
+  }, [pagenum, search, pagenum2, numpage2]);
   const Img = styled("img")({
     margin: "auto",
     display: "block",
     maxWidth: "100%",
     maxHeight: "100%",
   });
-  console.log(bookinfo.length);
+  // console.log(bookinfo.length);
 
-  const handlePagination = (event) => {
-    setpagenum(event.target.textContent);
-    console.log(event.target.textContent + "***");
-    history.replace(`/Book/${event.target.textContent}`);
+  const handlePagination = (e, p) => {
+    setpagenum(p);
+    console.log(p);
+    history.replace(`/Book/page=${p}`);
+    window.scroll(0, 0);
+  };
+  const handlePagination2 = (e, p) => {
+    setpagenum2(p);
+    console.log(p);
+    history.replace(`/Book/search/?q=${searchUrlParam}&page=${p}`);
+    window.scroll(0, 0);
   };
   let numbook = bookinfo.length;
 
@@ -160,87 +179,109 @@ const Showbook = () => {
           </div>
         )}
         {!apiLoading && (
-          <div
-            style={{
-              direction: "rtl",
-              display: "flex",
-              justifyContent: "center",
-              marginBottom: "50px",
-            }}
-          >
-            <Grid
-              item
-              container
-              sx={{
+          <>
+            <div
+              style={{
+                direction: "rtl",
                 display: "flex",
                 justifyContent: "center",
-                flexWrap: "wrap",
+                marginBottom: "50px",
               }}
             >
-              {bookinfo.map((info, index) => (
-                <Grid
-                  className="showbookall_paper"
-                  key={index}
-                  to={`/bookinfo/${info.id}`}
-                  component={Link}
-                  sx={{
-                    direction: "rtl",
-                  }}
-                  style={{
-                    marginTop: "40px",
-                    textDecoration: "none",
-                    display: "flex",
-                    alignItems: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <ButtonBase style={{ display: "flex", alignItems: "center" }}>
-                    <img
-                      alt="complex1"
-                      className="showbookall_img"
-                      src={info.image_url}
-                      style={{
-                        marginTop: "20px",
-                        height: "150px",
-                        width: "120px",
-                      }}
-                    />
-                  </ButtonBase>
+              <Grid
+                item
+                container
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexWrap: "wrap",
+                }}
+              >
+                {bookinfo.map((info, index) => (
+                  <Grid
+                    className="showbookall_paper"
+                    key={index}
+                    to={`/bookinfo/${info.id}`}
+                    component={Link}
+                    sx={{
+                      direction: "rtl",
+                    }}
+                    style={{
+                      marginTop: "40px",
+                      textDecoration: "none",
+                      display: "flex",
+                      alignItems: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <ButtonBase
+                      style={{ display: "flex", alignItems: "center" }}
+                    >
+                      <img
+                        alt="complex1"
+                        className="showbookall_img"
+                        src={info.image_url}
+                        style={{
+                          marginTop: "20px",
+                          height: "150px",
+                          width: "120px",
+                        }}
+                      />
+                    </ButtonBase>
 
-                  <div
-                    className="showbookall_name"
-                    variant="subtitle1"
-                    component="div"
-                    style={{ color: "black" }}
-                  >
-                    {info.name}
-                  </div>
-                  <div
-                    variant="body2"
-                    style={{ color: "#757C86", fontSize: "13px" }}
-                    className="showbookall_author"
-                  >
-                    {info.author}
-                  </div>
-                </Grid>
-              ))}
-            </Grid>
+                    <div
+                      className="showbookall_name"
+                      variant="subtitle1"
+                      component="div"
+                      style={{ color: "black" }}
+                    >
+                      {info.name}
+                    </div>
+                    <div
+                      variant="body2"
+                      style={{ color: "#757C86", fontSize: "13px" }}
+                      className="showbookall_author"
+                    >
+                      {info.author}
+                    </div>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          </>
+        )}
+        {searchUrlParam === null && numpage > 1 ? (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Pagination
+              count={numpage}
+              style={{
+                marginTop: "-30px",
+                marginBottom: "30px",
+                padding: "10px 80px",
+              }}
+              variant="outlined"
+              color="primary"
+              onChange={handlePagination}
+            />
           </div>
+        ) : (
+          <>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Pagination
+                count={numpage2}
+                style={{
+                  marginTop: "-30px",
+                  marginBottom: "30px",
+                  padding: "10px 80px",
+                }}
+                variant="outlined"
+                color="primary"
+                onChange={handlePagination2}
+              />
+            </div>
+          </>
         )}
       </div>
-      {searchUrlParam === null ? (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Pagination
-            count={3}
-            style={{ marginTop: "-30px", marginBottom: "30px" }}
-            variant="outlined"
-            color="primary"
-            onChange={handlePagination}
-          />
-        </div>
-      ) : (
-        <></>
-      )}
     </div>
   );
 };
