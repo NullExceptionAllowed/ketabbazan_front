11e27b68513593067e04312598e38798935932a8
FatiAs <fatemehaskarijirhandeh@gmail.com>
2022-06-09 09:34:15 +0430

    fix url

--- src/Components/Profile/components/AddPhoto.jsx
+++ src/Components/Profile/components/AddPhoto.jsx
@@ -1,76 +1,65 @@
 import React from "react";
 import "../Profile.css";
 import axios from "axios";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { SettingsApplicationsTwoTone } from "@mui/icons-material";
-
-
+import { baseUrl } from "./../../Variable";
 
 const AddPhoto = () => {
+  const [selectedFile, setSelectedFile] = React.useState("");
+  console.log("hellsdjfkljsdfksdklghisdjgklsdjgklsdhgjls");
 
-    const [selectedFile, setSelectedFile] = React.useState('');
-    console.log('hellsdjfkljsdfksdklghisdjgklsdjgklsdhgjls');
+  let token = "Token " + localStorage.getItem("token");
 
-    let token = "Token " + localStorage.getItem('token');
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    console.log("event :", event.target.files[0]);
 
-    const onFileChange = event => {
-        setSelectedFile(event.target.files[0]);
-        console.log('event :', event.target.files[0]);
+    console.log("pic ", selectedFile);
+  };
 
-        console.log('pic ', selectedFile);
-    }
+  const onFileUpload = () => {
+    // Create an object of formData
+    const formData = new FormData();
 
-    const onFileUpload = () => {
+    // Update the formData object
+    formData.append("image", selectedFile, selectedFile.name);
+    // const res =  await apiFUnction(file);
 
-        // Create an object of formData
-        const formData = new FormData();
+    // Details of the uploaded file
+    console.log(selectedFile);
 
-        // Update the formData object
-        formData.append(
-            "image",
-            selectedFile,
-           selectedFile.name
-        );
-        // const res =  await apiFUnction(file);
-
-        // Details of the uploaded file
-        console.log(selectedFile);
-
-        // Request made to the backend api
-        // Send formData object
-        axios.post("http://127.0.0.1:8000/profile/image/", formData,{
+    // Request made to the backend api
+    // Send formData object
+    axios
+      .post(`${baseUrl}/profile/image/`, formData, {
         headers: {
-            'content-type': 'application/json ',
-            'Authorization': token
-         }}
-        ).then(
-            (res)  => {
-                console.log(res.status);
-            }
-        );
-    };
+          "content-type": "application/json ",
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        console.log(res.status);
+      });
+  };
 
-    return (
-        <div className="root">
-            <div className="section addPhoto">
+  return (
+    <div className="root">
+      <div className="section addPhoto">
+        <form>
+          <input type="file" onChange={onFileChange} />
 
-                <form>
-                    <input type="file" onChange={onFileChange} />
-                    
-                    <Button onClick={onFileUpload}  style={{ width: "160px", marginRight: "600px", marginTop: "55px" }}
-                     variant="contained">تغییر عکس</Button>
-                </form>
-
-            </div>
- 
-
-        </div>
-
-
-
-    );
-
-
+          <Button
+            onClick={onFileUpload}
+            style={{ width: "160px", marginRight: "600px", marginTop: "55px" }}
+            variant="contained"
+          >
+            تغییر عکس
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddPhoto;
