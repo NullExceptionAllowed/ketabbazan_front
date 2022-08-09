dce89928e31fdd8bcc952f2a1cf77bd8133e4663
FatemehAskari <fatemeaskari2000@yahoo.com>
2022-08-09 22:29:43 +0430

    fix

--- src/Variable.js
+++ src/Variable.js
@@ -1,7 +1,7 @@
 //export const baseUrl = 'http://derakhshan.pythonanywhere.com';
 //export const baseUrl = 'http://94.101.185.252';
-//export const baseUrl = 'https://api.ketabbazan.ml';
-export const baseUrl = 'https://ahder.pythonanywhere.com';
+export const baseUrl = 'https://api.ketabbazan.ml';
+//export const baseUrl = 'https://ahder.pythonanywhere.com';
 //export const baseUrl = 'ahder.pythonanywhere.com';
 //export const baseUrl = 'http://98522148.pythonanywhere.com';
 //export const baseUrl = 'http://127.0.0.1:8000/';
