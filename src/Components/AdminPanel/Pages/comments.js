a7a6e0204d8652e400ea273a054f4c1d36ff05d4
amirhoseinmjt <am8896626@gmail.com>
2023-05-01 19:09:05 +0430

    admin panel comments indexing page and api handel done

--- src/Components/AdminPanel/Pages/comments.js
+++ src/Components/AdminPanel/Pages/comments.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './commentsPage.css';
 import SideBar from "../SideBar/SideBar";
 import Nav from "../../Navbar/Nav";
 import Nav2 from "../../Navbar/Nav2";
 import AboutUs from "../../AboutUs/AboutUs";
+import axios from "axios";
+import {baseUrl} from "../../../Variable";
+import VeriFyArticles from "./VeriFyArticles";
+import ValidateComment from "./ValidateComment";
+import AddandDelUser from "../../RootPanel/Pages/AddandDelUser";
 
 
 
@@ -30,15 +35,45 @@ const ChangeNav = () => {
     );
 }
 function Comments(props) {
+    const [cmnts, setcmnts] = React.useState(["کامنتی وجود ندارد"])
+
+    let token = "Token " + localStorage.getItem('token');
+
+    const [show,setshow] = useState();
+
+    React.useEffect(() => {
+        axios.get(`${baseUrl}/admin-panel/comment`, {
+            headers: {
+                'Content-Type': 'application/json ',
+                "Authorization": token
+            }
+        }).then((response) => {
+            setcmnts(response.data);
+            console.log("-------------------------comments :",response.data)
+            setshow(<>
+                {
+                    response.data.map(
+                        (cmnt) => {
+                            return <ValidateComment  comment={cmnt} />
+                        }
+                    )
+                }
+            </>)
+            //console.log(response.data);
+        });
+    }, [])
+
+    const test = ()=>{
+        return show;
+    }
     return (
         <>
             <ChangeNav></ChangeNav>
             <SideBar />
             <div className="Admin_comments_page">
-
-
-
-                <p>cmnt</p>
+                {
+                    test()
+                }
             </div>
         </>
 
