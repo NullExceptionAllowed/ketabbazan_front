65755665cfcfc74d3de0b19276d12bade032e0ee
Amin Mirabadi <53972620+97522247@users.noreply.github.com>
2023-06-03 18:21:09 +0000

    chat created

---
+++ src/Components/Chat/fakeres.js
@@ -0,0 +1,224 @@
+const fakeres = [
+    {
+      id: 1,
+      category: {
+        name: "داستانی",
+        id: 8,
+      },
+      owner: {
+        id: 23,
+        username: "u995981",
+        profile: {
+          image:
+            "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+        },
+      },
+      users: [
+        {
+          id: 3,
+          username: "u975952",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 10,
+          username: "u884469",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 23,
+          username: "u995981",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+      ],
+      name: "گروه رمان خوانی",
+      bio: "این گروه برای مطالعه رمان های معروف دنیا ساخته شده است",
+      picture: "http://194.33.105.191:8000/media/groupimages/default.jpg",
+    },
+    {
+      id: 2,
+      category: {
+        name: "نمایش",
+        id: 9,
+      },
+      owner: {
+        id: 23,
+        username: "u995981",
+        profile: {
+          image:
+            "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+        },
+      },
+      users: [
+        {
+          id: 3,
+          username: "u975952",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 10,
+          username: "u884469",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 23,
+          username: "u995981",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+      ],
+      name: "گروه رمان خوانی",
+      bio: "این گروه برای مطالعه رمان های معروف دنیا ساخته شده است",
+      picture: "http://194.33.105.191:8000/media/groupimages/default.jpg",
+    },
+    {
+      id: 3,
+      category: {
+        name: "فلسفه",
+        id: 10,
+      },
+      owner: {
+        id: 23,
+        username: "u995981",
+        profile: {
+          image:
+            "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+        },
+      },
+      users: [
+        {
+          id: 3,
+          username: "u975952",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 10,
+          username: "u884469",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 23,
+          username: "u995981",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+      ],
+      name: "گروه رمان خوانی",
+      bio: "این گروه برای مطالعه رمان های معروف دنیا ساخته شده است",
+      picture: "http://194.33.105.191:8000/media/groupimages/default.jpg",
+    },
+    {
+      id: 4,
+      category: {
+        name: "تاریخ",
+        id: 11,
+      },
+      owner: {
+        id: 23,
+        username: "u995981",
+        profile: {
+          image:
+            "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+        },
+      },
+      users: [
+        {
+          id: 3,
+          username: "u975952",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 10,
+          username: "u884469",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 23,
+          username: "u995981",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+      ],
+      name: "گروه رمان خوانی",
+      bio: "این گروه برای مطالعه رمان های معروف دنیا ساخته شده است",
+      picture: "http://194.33.105.191:8000/media/groupimages/default.jpg",
+    },
+    {
+      id: 5,
+      category: {
+        name: "فلسفه",
+        id: 10,
+      },
+      owner: {
+        id: 23,
+        username: "u995981",
+        profile: {
+          image:
+            "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+        },
+      },
+      users: [
+        {
+          id: 3,
+          username: "u975952",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 10,
+          username: "u884469",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+        {
+          id: 23,
+          username: "u995981",
+          profile: {
+            image:
+              "http://194.33.105.191:8000/media/media/profileimages/default.jpg",
+          },
+        },
+      ],
+      name: "گروه رمان خوانی",
+      bio: "این گروه برای مطالعه رمان های معروف دنیا ساخته شده است",
+      picture: "http://194.33.105.191:8000/media/groupimages/default.jpg",
+    },
+  ];
+  
+  export default fakeres;
